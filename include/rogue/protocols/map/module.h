8f12432e30ed68b935fdccdff30ab59cf2ecd6ac
Ryan Herbst <rherbst@slac.stanford.edu>
2016-09-20 22:00:57 -0700

    Added python sub-modules.

---
+++ include/rogue/protocols/map/module.h
@@ -0,0 +1,34 @@
+/**
+ *-----------------------------------------------------------------------------
+ * Title      : Python Module
+ * ----------------------------------------------------------------------------
+ * File       : module.h
+ * Author     : Ryan Herbst, rherbst@slac.stanford.edu
+ * Created    : 2016-08-08
+ * Last update: 2016-08-08
+ * ----------------------------------------------------------------------------
+ * Description:
+ * Python module setup
+ * ----------------------------------------------------------------------------
+ * This file is part of the rogue software platform. It is subject to 
+ * the license terms in the LICENSE.txt file found in the top-level directory 
+ * of this distribution and at: 
+ *    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+ * No part of the rogue software platform, including this file, may be 
+ * copied, modified, propagated, or distributed except according to the terms 
+ * contained in the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+**/
+#ifndef __ROGUE_PROTOCOLS_MAP_MODULE_H__
+#define __ROGUE_PROTOCOLS_MAP_MODULE_H__
+
+namespace rogue {
+   namespace protocols {
+      namespace map {
+         void setup_module();
+      }
+   }
+}
+
+#endif
+
