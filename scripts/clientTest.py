85745c82f6ba2767b03b1c4b7e81769f94830b07
Ryan T. Herbst <rherbst@slac.stanford.edu>
2016-10-11 17:58:43 -0700

    Added client gui test.

---
+++ scripts/clientTest.py
@@ -0,0 +1,11 @@
+import pyrogue.mesh
+import pyrogue.gui
+import PyQt4.QtGui
+import sys
+
+# Create mesh node
+node = pyrogue.mesh.MeshNode('rogue')
+
+appTop = PyQt4.QtGui.QApplication(sys.argv)
+#guiTop = pyrogue.gui.GuiTop(evalBoard)
+
