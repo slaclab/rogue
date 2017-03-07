1d6087b5ddd6022c47c4bbdf91f7c4456ffc6b7c
Benjamin Reese <bareese@slac.stanford.edu>
2017-03-07 10:25:05 -0800

    Debug displays 8 bytes per line instead of 10

--- src/rogue/interfaces/stream/Slave.cpp
+++ src/rogue/interfaces/stream/Slave.cpp
@@ -69,7 +69,7 @@ void ris::Slave::acceptFrame ( ris::FramePtr frame ) {
          frame->read(&val,x,1);
 
          sprintf(buffer + strlen(buffer)," 0x%.2x",val);
-         if (( (x+1) % 10 ) == 0) {
+         if (( (x+1) % 8 ) == 0) {
             log.log("info",buffer);
             sprintf(buffer,"     ");
          }
