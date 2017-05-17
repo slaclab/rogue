21f8277732d14f2a8b2f345a4632b537b9f18fad
Ryan Herbst <rherbst@slac.stanford.edu>
2017-05-16 20:38:09 -0700

    pulled gui from Ben's branch

--- python/pyrogue/gui/variables.py
+++ python/pyrogue/gui/variables.py
@@ -37,27 +37,21 @@ class VariableLink(QObject):
         parent.addChild(item)
         item.setText(0,variable.name)
         item.setText(1,variable.mode)
-        item.setText(2,variable.base)
+        item.setText(2,variable.disp) # Fix this. Should show base and size
 
         if variable.units:
            item.setText(4,str(variable.units))
 
-        if variable.base == 'enum' and variable.mode=='RW':
+        if variable.disp == 'enum' and variable.enum is not None and variable.mode=='RW':
+            #print('VariableLink: variable: {} , enum: {}'.format(variable, variable.enum))
             self.widget = QComboBox()
             self.widget.activated.connect(self.guiChanged)
             self.connect(self,SIGNAL('updateGui'),self.widget.setCurrentIndex)
 
-            for i in sorted(variable.enum):
+            for i in variable.enum:
                 self.widget.addItem(variable.enum[i])
 
-        elif variable.base == 'bool' and variable.mode=='RW':
-            self.widget = QComboBox()
-            self.widget.addItem('False')
-            self.widget.addItem('True')
-            self.widget.activated.connect(self.guiChanged)
-            self.connect(self,SIGNAL('updateGui'),self.widget.setCurrentIndex)
-
-        elif variable.base == 'range':
+        elif variable.disp == 'range':
             self.widget = QSpinBox();
             self.widget.setMinimum(variable.minimum)
             self.widget.setMaximum(variable.maximum)
@@ -74,65 +68,33 @@ class VariableLink(QObject):
 
         item.treeWidget().setItemWidget(item,3,self.widget)
         variable.addListener(self.newValue)
-        self.newValue(None,variable.get(read=False))
-
-    def newValue(self,var,value):
-        if self.block: return
-
-        if self.variable.mode=='RW' and (self.variable.base == 'enum' or self.variable.base == 'bool'):
-            self.emit(SIGNAL("updateGui"),self.widget.findText(str(value)))
-
-        elif self.variable.base == 'range':
-            self.emit(SIGNAL("updateGui"),value)
-
-        elif self.variable.base == 'hex' and value != None:
-            self.emit(SIGNAL("updateGui"),'0x%x' % (value))
-
-        elif self.variable.base == 'bin' and value != None:
-            self.emit(SIGNAL("updateGui"), '0b{0:b}'.format(value))
-
-        elif self.variable.base == 'float' or self.variable.base == 'string':
-            self.emit(SIGNAL("updateGui"), str(value))
-
+        
+        if isinstance(self.widget, QComboBox):
+            self.newValue(None,self.widget.findText(variable.getDisp(read=False)))
         else:
-            self.emit(SIGNAL("updateGui"), str(value))
-            #self.emit(SIGNAL("updateGui"), self.variable.base.format(value)) # Broken
-
+            self.newValue(None,variable.getDisp(read=False))
+            
+    def newValue(self, var, value):
+        #print('{} newValue ( {} {} )'.format(self.variable, type(value), value))
+        if self.block: return
+        self.emit(SIGNAL("updateGui"), value)
 
     def returnPressed(self):
         self.guiChanged(self.widget.text())
 
-    def guiChanged(self,value):
+    def guiChanged(self, value):
+        #print('{} guiChanged( {}({}) )'.format(self.variable, type(value), value))
         self.block = True
 
-        if self.variable.base == 'enum':
-            self.variable.set(str(self.widget.itemText(value)))
-
-        elif self.variable.base == 'bool':
-            self.variable.set(self.widget.itemText(value) == 'True')
-
-        elif self.variable.base == 'range':
-            self.variable.set(value)
-
-        elif self.variable.base == 'hex':
-            self.variable.set(int(str(value),16))
-
-        elif self.variable.base == 'uint':
-            self.variable.set(int(str(value)))
-
-        elif self.variable.base == 'bin':
-            self.variable.set(int(str(value), 2))
-
-        elif self.variable.base == 'float':
-            self.variable.set(float(str(value)))
-                      
-        elif self.variable.base == 'string':
-            self.variable.set(str(value))
+        if self.variable.disp == 'enum':
+            # For enums, value will be index of selected item
+            #print('indexDict({}) = {}'.format(value, self.indexDict[value]))
+            self.variable.setDisp(self.widget.itemText(value))
 
         else:
-            #self.variable.set(parse.parse(self.variable.base, value)[0])  # Broken
-            self.variable.set(str(value))
-            
+            # For non enums, value will be string entered in box
+            self.variable.setDisp(value)
+
         self.block = False
 
 
