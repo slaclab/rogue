fcbc3122f55e8f2009135c47c4aa9c27918c2470
Benjamin Reese <bareese@slac.stanford.edu>
2017-07-06 14:52:41 -0700

    Cleanup block creation (still broken)

--- python/pyrogue/_Memory.py
+++ python/pyrogue/_Memory.py
@@ -1,43 +1,42 @@
 import pyrogue as pr
 
-class PseudoVariable(object):
-    def __init__(self, device, offset=0):
-        self.device = device
-        self.offset = offset
-
 class MemoryDevice(pr.Device):
-    def __init__(self, base=pr.UInt, wordSize=4, stride=4, **kwargs):
+    def __init__(self, base=pr.UInt, wordSize=4, stride=4, verify=True, **kwargs):
         super().__init__(hidden=True, **kwargs)
 
         self._base = base
         self._wordSize = wordSize
         self._bitSize = wordSize*8
         self._stride = stride
-        
-        self.add(pr.LocalVariable(name='Verify', value=True))
+        self._bitStride = stride*8
+        self._verify = verify
 
     def _buildBlocks(self):
-        self._blocks = [pr.RawBlock(self)]
+        pass
 
     def _setOrExec(self, d, writeEach, modes):
         """ Spin up a number of MemoryBlocks and set() them """
-        for key, value in d.items():
-            values = self._base.fromString(i) for i in value.split(',')
 
-            block = pr.MemoryBlock(self, offset=key)
-            self._blocks.append(block)
-            block.set(values)
-
+        for offset, valueStr in d.items():
+            values = self._base.fromString(i) for i in valueStr.split(',')
+            size = len(values) * self._stride
+            blockSize = self._reqMaxAccess()
+            for i in range(offset, offset+size, blockSize):
+                block = pr.MemoryBlock(self, offset=i)                
+                self._blocks.append(block)
+                block.set(values[offset-i:min(len(values), offset-i+blockSize)])
             
-#             blockSize = self._reqMaxAccess()
-#             numBytes = len(values) * self._stride
-#             numBlocks = wordCount(len(values)*self._wordSize, blockSize)
 
+    def checkBlocks(self, varUpdate=True, recurse=True, variable=None):
+        Device.checkBlocks(varUpdate=varUpdate, recurse=recurse, variable=variable)
 
-    def verifyBlocks(self, recurse=True, variable=None):
-        Device.verifyBlocks(self, recurse=recurse, variable=variable)
-        # After verify is done, delete the Block objects
-        self._blocks = []
+        self._blocks = [b for b in self._blocks if b._verifyWr is False]
 
     def readBlocks(self, recurse=True, variable=None):
+        pass
+
+    def write(self, offset, values):
+        pass
+
+    def read(self, offset, size):
         pass
