4a8ef53e0b68eb9b23589a8665b7f9c6978d6ae3
Benjamin Reese <bareese@slac.stanford.edu>
2018-02-08 17:01:31 -0800

    Add locking

--- python/pyrogue/_Block.py
+++ python/pyrogue/_Block.py
@@ -442,13 +442,14 @@ class RemoteBlock(BaseBlock, rim.Master):
             return True
 
     def _setDefault(self, var, value):
-        # Stage the default data
-        self.set(var, value)
+        with self._lock:
+            # Stage the default data        
+            self.set(var, value)
 
-        # Move stage data to block, but keep it staged as well
-        for x in range(self._size):
-            self._bData[x] = self._bData[x] & (self._sDataMask[x] ^ 0xFF)
-            self._bData[x] = self._bData[x] | (self._sDataMask[x] & self._sData[x])
+            # Move stage data to block, but keep it staged as well
+            for x in range(self._size):
+                self._bData[x] = self._bData[x] & (self._sDataMask[x] ^ 0xFF)
+                self._bData[x] = self._bData[x] | (self._sDataMask[x] & self._sData[x])
 
 
     def updated(self):
