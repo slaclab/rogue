d9076fe7edf11acd7b30f3b6638319446633cfc7
Ryan Herbst <rherbst@slac.stanford.edu>
2018-04-02 23:36:04 -0700

    Import try re-write

--- python/pyrogue/gui/system.py
+++ python/pyrogue/gui/system.py
@@ -22,7 +22,7 @@ try:
     from PyQt5.QtWidgets import *
     from PyQt5.QtCore    import *
     from PyQt5.QtGui     import *
-except:
+except ImportError:
     from PyQt4.QtCore    import *
     from PyQt4.QtGui     import *
 
