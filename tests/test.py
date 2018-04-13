32afae26aae58cb51a7f52aa22520ab2f8232fa5
Jesus Vasquez <jesusvasquez333@gmail.com>
2018-04-13 00:27:39 -0700

    Run a first test

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
 import pyrogue
+import rogue
+
+class myDevice(pyrogue.Device):
+    def __init__(self, name="myDevice", description='My device', **kargs):
+        super().__init__(name=name, description=description, **kargs)
+
+        self.add(pyrogue.LocalVariable(
+            name='var',
+            value=3.14,
+            mode='RW'))
+
+class LocalRoot(pyrogue.Root):
+    def __init__(self):
+        pyrogue.Root.__init__(self, name='LocalRoot', description='Local root')
+        my_device = myDevice()
+        self.add(my_device)
+
+if __name__ == "__main__":
+    root = LocalRoot()
+    root.start()
+
+    print(root.myDevice.var.get())
+
+    root.stop()
