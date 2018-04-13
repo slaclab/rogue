e614c7e21035ed8e4f8e36732ca491078bc21214
Jesus Vasquez <jesusvasquez333@gmail.com>
2018-04-13 09:31:32 -0700

    Let's try a code coverage test

---
+++ tests/cov_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import pyrogue
+import rogue
+import unittest
+
+class myDevice(pyrogue.Device):
+    def __init__(self, name="myDevice", description='My device', **kargs):
+        super().__init__(name=name, description=description, **kargs)
+
+        self.add(pyrogue.LocalVariable(
+            name='var',
+            value=3.14,
+            mode='RW'))
+
+class LocalRoot(pyrogue.Root):
+    def __init__(self):
+        pyrogue.Root.__init__(self, name='LocalRoot', description='Local root')
+        my_device = myDevice()
+        self.add(my_device)
+
+class Root(unittest.TestCase):
+    """
+    Test Pyrogue
+    """
+    root = LocalRoot()
+    root.start()
+
+    print(root.myDevice.var.get())
+
+    root.stop()
+
+if __name__ == "__main__":
+    unittest.main()
