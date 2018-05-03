54308a57a542fd517bb59648b6d9a874fa198b49
Ryan Herbst <rherbst@slac.stanford.edu>
2018-05-02 17:09:28 -0700

    Update buffer pool interfaces

--- src/rogue/protocols/udp/Core.cpp
+++ src/rogue/protocols/udp/Core.cpp
@@ -40,9 +40,11 @@ uint32_t rpu::Core::maxPayload() {
 }
 
 //! Set UDP RX Size
-bool rpu::Core::setRxSize(uint32_t size) {
+bool rpu::Core::setRxBufferCount(uint32_t count) {
    uint32_t   rwin;
    socklen_t  rwin_size=4;
+
+   uint32_t size = count * (jumbo_)?(JumboMTU):(StdMTU);
 
    setsockopt(fd_, SOL_SOCKET, SO_RCVBUF, (char*)&size, sizeof(size));
    getsockopt(fd_, SOL_SOCKET, SO_RCVBUF, &rwin, &rwin_size);
@@ -65,7 +67,7 @@ void rpu::Core::setTimeout(uint32_t timeout) {
 void rpu::Core::setup_python () {
    bp::class_<rpu::Core, rpu::CorePtr, boost::noncopyable >("Core",bp::no_init)
       .def("maxPayload",        &rpu::Core::maxPayload)
-      .def("setRxSize",         &rpu::Core::setRxSize)
+      .def("setRxBufferCount",  &rpu::Core::setRxBufferCount)
       .def("setTimeout",        &rpu::Core::setTimeout)
    ;
 }
