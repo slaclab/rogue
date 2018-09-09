6505133542aa7fbe70a39313766997057762ea1d
Jesus Vasquez <jesusvasquez333@gmail.com>
2018-09-08 23:00:33 -0700

    Test one local variable

---
+++ tests/test_localvars.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pyrogue
+
+class myDevice(pyrogue.Device):
+    def __init__(self, name="myDevice", description='My device', **kargs):
+        super().__init__(name=name, description=description, **kargs)
+
+        self.add(pyrogue.LocalVariable(
+            name='var',
+            value=3.14,
+            mode='RW'))
+
+class LocalRoot(pyrogue.Root):
+    def __init__(self):
+        pyrogue.Root.__init__(self, name='LocalRoot', description='Local root')
+        my_device = myDevice()
+        self.add(my_device)
+
+def test_local_root():
+    """
+    Test Pyrogue
+    """
+    root = LocalRoot()
+    root.start()
+    result = root.myDevice.var.get()
+    root.stop()
+    if result != 3.14:
+        raise AssertionError()
