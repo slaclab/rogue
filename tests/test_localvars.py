2c811ee132a4b62555493926f76e3f95414dafef
Jesus Vasquez <jesusvasquez333@gmail.com>
2018-09-12 17:30:48 -0700

    Add RW arrays and LinkVariables to the tests

--- tests/test_localvars.py
+++ tests/test_localvars.py
@@ -8,8 +8,27 @@ class myDevice(pyrogue.Device):
 
         self.add(pyrogue.LocalVariable(
             name='var',
-            value=3.14,
+            value=0.0,
             mode='RW'))
+
+        self.add(pyrogue.LocalVariable(
+            name='var_array',
+            value=[0]*5,
+            mode='RW'))
+
+        self.add(pyrogue.LinkVariable(
+            name='var_link',
+            mode='RW',
+            linkedSet=lambda dev, var, value: var.dependencies[0].set(value),
+            linkedGet=lambda dev, var: var.dependencies[0].value(),
+            dependencies=[self.variables['var']]))
+
+        self.add(pyrogue.LinkVariable(
+            name='var_array_link',
+            mode='RW',
+            linkedSet=lambda dev, var, value: var.dependencies[0].set(value),
+            linkedGet=lambda dev, var: var.dependencies[0].value(),
+            dependencies=[self.variables['var_array']]))
 
 class LocalRoot(pyrogue.Root):
     def __init__(self):
@@ -29,10 +48,31 @@ def test_local_root():
     Test Pyrogue
     """
     with LocalRoot() as root:
-        
+
         # Test RW a variable holding an scalar value
         test_value=3.14
         root.myDevice.var.set(test_value)
         test_result=root.myDevice.var.get()
+        if test_result != test_value:
+            raise AssertionError()
+
+        # Test RW a variable holding an array value
+        test_value=[1, 2, 3, 4, 5]
+        root.myDevice.var_array.set(test_value)
+        test_result=root.myDevice.var_array.get()
+        if test_result != test_value:
+            raise AssertionError()
+
+        # Test RW a link variable holding an scalue value
+        test_value=5.6
+        root.myDevice.var_link.set(test_value)
+        test_result=root.myDevice.var_link.get()
+        if test_result != test_value:
+            raise AssertionError()
+
+        # Test RW a link variable holding an array value
+        test_value=[6, 7, 8, 9, 10]
+        root.myDevice.var_array_link.set(test_value)
+        test_result=root.myDevice.var_array_link.get()
         if test_result != test_value:
             raise AssertionError()
