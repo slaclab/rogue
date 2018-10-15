20fc2b62d26e7daf85ed0739cf7658a34e649640
Ryan Herbst <rherbst@slac.stanford.edu>
2018-10-14 21:12:55 -0700

    Update

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,10 +2,6 @@
 
 mkdir build
 cd build
-cmake .. -DROGUE_INSTALL=conda -DROGUE_DIR=${PREFIX}
+cmake .. -DROGUE_INSTALL=system -DROGUE_DIR=${PREFIX}
 make -j ${CPU_COUNT} install
-
-#$PYTHON setup.py install --single-version-externally-managed
-
-$PYTHON setup.py install
 
