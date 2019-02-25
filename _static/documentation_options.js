c2c288954c222e686123735dd584f6ae2d0558be
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-02-25 20:15:27 +0000

    Update docs after building Travis build 433 of slaclab/rogue
    The docs were built from the branch 'documentation' against the commit
    48e95f363bed559170f805d8055214309ae08fee.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/slaclab/rogue/jobs/498359840.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages --branch-whitelist documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v3.3.1-37-gfc56724a',
+    VERSION: 'v3.3.1-40-g48e95f36',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
