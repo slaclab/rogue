ce51f7095ab3d06d190a44a9616b6792581991fa
Benjamin Reese <bareese@slac.stanford.edu>
2019-04-04 14:41:08 -0700

    Fix typos and add debugging

--- python/pyrogue/protocols/_uart.py
+++ python/pyrogue/protocols/_uart.py
@@ -22,11 +22,11 @@ import pyrogue
 import serial
 
 class UartMemory(rogue.interfaces.memory.Slave):
-    def __init__(self, device, *args, timeout=1, **kwargs):
-        super()__init__(4,4) # Set min and max size to 4 bytes
+    def __init__(self, device, timeout=1, **kwargs):
+        super().__init__(4,4) # Set min and max size to 4 bytes
 
         self._log = pyrogue.logInit(cls=self, name=f'{device}')
-        self.serialPort = serial.Serial(device, *args, timeout=timeout, **kwargs)
+        self.serialPort = serial.Serial(device, timeout=timeout, **kwargs)
 
     def __enter__(self):
         return self
@@ -44,13 +44,16 @@ class UartMemory(rogue.interfaces.memory.Slave):
                 transaction.getData(data, 0)
                 data = int.from_bytes(data, 'little', signed=False)
                 sendString = f'w {transaction.address():08x} {data:08x} \n'
+                self._log.debug(f'Sending write transaction: {repr(sendString)}')
                 
             elif transaction.type() == rogue.interfaces.memory.Read:
                 sendString = f'r {transaction.address():08x} \n'
+                self._log.debug(f'Sending read transaction: {repr(sendString)}')
 
             else:
                 # Posted writes not supported (for now)
                 transaction.done(rogue.interfaces.memory.Unsupported)
+                self._log.error(f'Unsupported transaction type: {transaction.type()}')
                 return
                 
             # Send the command and wait for response
@@ -60,6 +63,7 @@ class UartMemory(rogue.interfaces.memory.Slave):
             # If response is empty, a timeout occured
             if len(response == 0):
                 transaction.done(rogue.interfaces.memory.TimeoutError)
+                self._log.error('Empty transaction response (likely timeout) for transaction: {repr(sendString)}')
                 return
 
             # parse the response string
@@ -70,16 +74,20 @@ class UartMemory(rogue.interfaces.memory.Slave):
                     parts[0].lower() != 'w' or
                     int(parts[1], 16) != transaction.address()):
                     transaction.done(rogue.interfaces.memory.ProtocolError)
+                    self._log.error('Malformed response: {repr(response)} to transaction: {repr(sendString)}')
                 else:
                     transaction.done(0)
+                    self._log.debug('Transaction: {repr(sendString)} completed successfully')
                     
             else: # if (transaction.type() == rogue.interfaces.memory.Read
                 if (len(parts) != 3 or
                     parts[0].lower() != 'r' or
                     int(parts[1], 16) != transaction.address()):
                     transaction.done(rogue.interfaces.memory.ProtocolError)
+                    self._log.error('Malformed response: {repr(response)} to transaction: {repr(sendString)}')                    
                 else:
                     rdData = int(parts[2], 16)
                     transaction.setData(rdData.to_bytes(4, 'little', signed=False))
                     transaction.done(0)
+                    self._log.debug('Transaction: {repr(sendString)} with response data: {rdData:#08x} completed successfully')
 
