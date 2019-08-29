68de2cf72eb1face0739585c86b396b3f30e5f62
Ryan Herbst <rherbst@slac.stanford.edu>
2019-08-29 09:10:41 -0700

    Fix size and indexing

--- python/pyrogue/utilities/cpsw.py
+++ python/pyrogue/utilities/cpsw.py
@@ -17,10 +17,9 @@
 # copied, modified, propagated, or distributed except according to the terms 
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
-import rogue.utilities
-import rogue.utilities.fileio
 import pyrogue
 import rogue
+import math
 
 def exportRemoteVariable(variable,indent):
 
@@ -28,42 +27,52 @@ def exportRemoteVariable(variable,indent):
         print(f"Error: Found variable {variable.path} with unsupported type {variable.typeStr}, error exporting")
         return ""
 
-    if len(variable.bitOffset) > 1:
-        print(f"Error: Found variable {variable.path} with multiple bit offsets, error exporting")
-        return ""
+    #if len(variable.bitOffset) > 1:
+    #    print(f"Warning: Splitting multiple bit offset variable {variable.path}")
+    size = 0
 
-    offset    = variable.offset + int(variable.bitOffset[0] / 8)
-    bitOffset = int(variable.bitOffset[0] % 8)
+    for i in range(len(variable.bitOffset)):
 
-    dat  = " " * indent +  "#########################################################\n"
-    dat += " " * indent + f"{variable.name}\n"
-    dat += " " * indent +  "at:\n"
-    dat += " " * indent + f"  offset: {offset:#x}\n"
-    dat += " " * indent +  "class: IntField\n"
+        offset    = variable.offset + int(variable.bitOffset[i] / 8)
+        bitOffset = int(variable.bitOffset[i] % 8)
 
-    if "String" in variable.typeStr:
-        dat += " " * indent +  "encoding: ASCII\n"
-        dat += " " * indent +  "sizebits: 8\n"
-        dat += " " * indent +  "nelms: {}\n".format(int(variable.bitSize[0]/8))
-    elif "Int" in variable.typeStr or 'Bool' in variable.typeStr:
-        dat += " " * indent + f"sizebits: {variable.bitSize[0]}\n"
-        dat += " " * indent + f"lsbit: {bitOffset}\n"
+        if (offset + math.ceil(variable.bitSize[i]/8)) > size:
+            size = offset + math.ceil(variable.bitSize[i]/8)
 
-        if variable.disp != '{}':
-            dat += " " * indent + "encoding: 16\n"
+        if len(variable.bitOffset) > 1:
+            name = f"{variable.name}[{i}]\n"
+        else:
+            name = f"{variable.name}\n"
 
-    dat += " " * indent + f"mode: {variable.mode}\n"
-    dat += " " * indent + f"description: {variable.description}\n"
+        dat  = " " * indent +  "#########################################################\n"
+        dat += " " * indent +  name
+        dat += " " * indent +  "at:\n"
+        dat += " " * indent + f"  offset: {offset:#x}\n"
+        dat += " " * indent +  "class: IntField\n"
 
-    if variable.enum is not None:
-        dat +=  " " * indent +  "enums:\n"
+        if "String" in variable.typeStr:
+            dat += " " * indent +  "encoding: ASCII\n"
+            dat += " " * indent +  "sizebits: 8\n"
+            dat += " " * indent +  "nelms: {}\n".format(int(variable.bitSize[0]/8))
+        elif "Int" in variable.typeStr or 'Bool' in variable.typeStr:
+            dat += " " * indent + f"sizebits: {variable.bitSize[0]}\n"
+            dat += " " * indent + f"lsbit: {bitOffset}\n"
 
-        for k,v in variable.enum.items():
-            dat += " " * indent + f"  - name: {v}\n"
-            dat += " " * indent +  "    class: Enum\n"
-            dat += " " * indent +  "    value: {}\n".format(int(k))
+            if variable.disp != '{}':
+                dat += " " * indent + "encoding: 16\n"
 
-    return dat
+        dat += " " * indent + f"mode: {variable.mode}\n"
+        dat += " " * indent + f"description: {variable.description}\n"
+
+        if variable.enum is not None:
+            dat +=  " " * indent +  "enums:\n"
+
+            for k,v in variable.enum.items():
+                dat += " " * indent + f"  - name: {v}\n"
+                dat += " " * indent +  "    class: Enum\n"
+                dat += " " * indent +  "    value: {}\n".format(int(k))
+
+    return dat,size
 
 def exportLinkVariable(variable, indent):
     dat  = " " * indent +  "#########################################################\n"
@@ -73,7 +82,7 @@ def exportLinkVariable(variable, indent):
     dat += " " * indent +  "#dependencies: {}\n".format(str(variable.dependencies))
     dat += " " * indent + f"#description: {variable.description}\n"
 
-    print(f"Warning: Found link variable {variable.path}")
+    #print(f"Warning: Found link variable {variable.path}")
     return dat
 
 def exportLocalVariable(variable, indent):
@@ -83,7 +92,7 @@ def exportLocalVariable(variable, indent):
     dat += " " * indent + f"#typeStr: {variable.typeStr}\n"
     dat += " " * indent + f"#description: {variable.description}\n"
 
-    print(f"Warning: Found local variable {variable.path}")
+    #print(f"Warning: Found local variable {variable.path}")
     return dat
 
 def exportCommand(command, indent):
@@ -92,29 +101,30 @@ def exportCommand(command, indent):
     dat += " " * indent + f"#{command.name}\n"
     dat += " " * indent + f"#description: {command.description}\n"
 
-    print(f"Warning: Found command {command.path}")
+    #print(f"Warning: Found command {command.path}")
     return dat
 
 def exportSubDevice(device, indent, deviceList, dataDir):
-    index = exportDevice(device, deviceList, dataDir)
-    dn = f"{device.__class__.__name__}_{index}"
+    index,size = exportDevice(device, deviceList, dataDir)
+
+    if index > 0:
+        dn = f"{device.__class__.__name__}_{index}"
+    else:
+        dn = f"{device.__class__.__name__}"
 
     dat  = " " * indent +  "#########################################################\n"
     dat += " " * indent + f"{device.name}\n"
     dat += " " * indent + f"    <<: *{dn}\n"
     dat += " " * indent +  "    at:\n"
-    dat += " " * indent + f"      offset: {device.offset}\n"
-    return dat,dn
+    dat += " " * indent + f"      offset: {device.offset:#x}\n"
+
+    size += device.offset
+    return dat,dn,size
 
 def exportDevice(device, deviceList, dataDir):
-    imp = []
-
-    dat  =  "  class: MMIODev\n"
-    dat +=  "  configPrio: 1\n"
-    dat += f"  description: {device.description}\n"
-    dat += f"  size: {device.size:#x}\n"
-    dat +=  "  children:\n"
-    dat +=  dat
+    imp  = []
+    size = 0
+    dat  = ""
 
     for k,node in device.nodes.items():
         if node.isinstance(pyrogue.EnableVariable):
@@ -122,14 +132,19 @@ def exportDevice(device, deviceList, dataDir):
         elif node.isinstance(pyrogue.LocalVariable):
             dat += exportLocalVariable(node,4)
         elif node.isinstance(pyrogue.RemoteVariable):
-            dat += exportRemoteVariable(node,4)
+            d,s = exportRemoteVariable(node,4)
+            dat += d
+            if s > size:
+                size = s
         elif node.isinstance(pyrogue.LinkVariable):
             dat += exportLinkVariable(node,4)
         elif node.isinstance(pyrogue.Command):
             dat += exportCommand(node,4)
         elif node.isinstance(pyrogue.Device):
-            ld,dn = exportSubDevice(node,4,deviceList, dataDir)
+            ld,dn,s = exportSubDevice(node,4,deviceList, dataDir)
             dat += ld
+            if s > size:
+                size = s
             imp.append(dn)
         else:
             print(f"Error: Unsupported node type {node.path}")
@@ -138,19 +153,29 @@ def exportDevice(device, deviceList, dataDir):
     prefix += "#Auto Generated CPSW Yaml File From pyrogue.utilities.cpsw.py                #\n"
     prefix += "##############################################################################\n"
     prefix += "#schemaversion 3.0.0\n"
+    include = ""
 
     for i in imp:
-        prefix += f"#once {i}.yaml\n"
-
-    prefix += "\n"
+        include += f"#include {i}.yaml\n"
 
     if device.__class__.__name__ not in deviceList:
         deviceList[device.__class__.__name__] = []
 
     for i in range(len(deviceList[device.__class__.__name__]) + 1):
-        tname = device.__class__.__name__ + f"_{i}"
+        if i > 0:
+            tname = device.__class__.__name__ + f"_{i}"
+        else:
+            tname = device.__class__.__name__
+
         ddat  = prefix
+        ddat +=  f"#once {tname}\n"
+        ddat +=  include + "\n"
         ddat +=  f"{tname}: &{tname}\n"
+        ddat  =  "  class: MMIODev\n"
+        ddat +=  "  configPrio: 1\n"
+        ddat += f"  description: {device.description}\n"
+        ddat += f"  size: {size:#x}\n"
+        ddat +=  "  children:\n"
         ddat +=  dat
 
         if i >= len(deviceList[device.__class__.__name__]):
@@ -161,7 +186,7 @@ def exportDevice(device, deviceList, dataDir):
     with open(dataDir + "/" + tname + ".yaml","w") as f:
         f.write(ddat)
 
-    return i
+    return i,size
 
 def exportRoot(root,dataDir):
     dlist = {}
