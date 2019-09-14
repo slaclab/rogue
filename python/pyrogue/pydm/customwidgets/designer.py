44bb1f534420c7066e6e40d5722cfbb7f8b8e7c7
Ryan Herbst <rherbst@slac.stanford.edu>
2019-09-13 22:36:21 -0700

    Update paths

--- python/pyrogue/pydm/customwidgets/designer.py
+++ python/pyrogue/pydm/customwidgets/designer.py
@@ -10,6 +10,6 @@
 
 from pydm.widgets.qtplugin_base import qtplugin_factory
 
-from .displays.rogue_widgets import RogueWidget
+from pyrogue.pydm.customwidgets.displays.rogue_widgets import RogueWidget
 
 ROGUEWidget = qtplugin_factory(RogueWidget, group="Rogue Widgets")
