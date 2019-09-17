357089185d4bfe1b4c51a71ebe4bb5f8ffd2e5d5
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-09-17 15:33:09 -0700

    Remove pthread_setname_np call from the UDP Client and Server threads

--- src/rogue/protocols/udp/Server.cpp
+++ src/rogue/protocols/udp/Server.cpp
@@ -82,9 +82,6 @@ rpu::Server::Server (uint16_t port, bool jumbo) : rpu::Core(jumbo) {
 
    // Start rx thread
    thread_ = new boost::thread(boost::bind(&rpu::Server::runThread, this));
-
-   // Set a thread name
-   pthread_setname_np( thread_->native_handle(), "UdpServer" );
 }
 
 //! Destructor
