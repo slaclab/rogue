22ae1449f681adc89941e4ca29ab25b6db5121ec
Ryan Herbst <rherbst@slac.stanford.edu>
2020-01-28 10:56:27 -0800

    Add block

---
+++ include/rogue/interfaces/memory/Block.h
@@ -0,0 +1,311 @@
+/**
+ *-----------------------------------------------------------------------------
+ * Title      : Memory Block
+ * ----------------------------------------------------------------------------
+ * File       : Block.h
+ * ----------------------------------------------------------------------------
+ * Description:
+ * Interface between RemoteVariables and lower level memory transactions.
+ * ----------------------------------------------------------------------------
+ * This file is part of the rogue software platform. It is subject to 
+ * the license terms in the LICENSE.txt file found in the top-level directory 
+ * of this distribution and at: 
+ *    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+ * No part of the rogue software platform, including this file, may be 
+ * copied, modified, propagated, or distributed except according to the terms 
+ * contained in the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+**/
+#ifndef __ROGUE_INTERFACES_MEMORY_BLOCK_H__
+#define __ROGUE_INTERFACES_MEMORY_BLOCK_H__
+#include <stdint.h>
+#include <vector>
+
+#include <rogue/interfaces/memory/Master.h>
+#include <thread>
+
+#ifndef NO_PYTHON
+#include <boost/python.hpp>
+
+namespace rogue {
+   namespace interfaces {
+      namespace memory {
+
+         class BlockVariable;
+
+         //! Memory interface Block device
+         /** 
+          */
+         class Block : public Master {
+
+               //! Mutex
+               std::mutex mtx_;
+
+               // Path
+               std::string path_;
+
+               // Mode
+               std::string mode_;
+
+               // Bulk Enable
+               bool bulkEn_;
+
+               // Verify Enable
+               bool verifyEn_;
+
+               // Verify Write
+               bool verifyWr_;
+
+               // verifyBase
+               uint32_t verifyBase_;
+
+               // verifyTop
+               uint32_t verifySize_;
+
+               // Staged data
+               uint8_t *stagedData_;
+
+               // Staged Mask
+               uint8_t *stagedMask_;
+
+               // Block data
+               uint8_t *blockData_;
+
+               // Verify data
+               uint8_t *verifyData_;
+
+               // Verify Mask
+               uint8_t *verifyMask_;
+
+               // Block size
+               uint32_t size_;
+
+               // Block offset
+               uint64_t offset_;
+
+               // List of variables
+               std::map<std::string, std::shared_ptr<rogue::interfaces::memory::BlockVariable> > variables_;
+
+               // Update Flag
+               bool doUpdate_;
+
+               // Block transaction flag
+               bool blockTrans_;
+
+               // Block logging
+               std::shared_ptr<rogue::Logging> bLog_;
+
+               // Overlap Enable
+               bool overlapEn_;
+
+            public:
+
+               //! Class factory which returns a pointer to a Block (BlockPtr)
+               /** Exposed to Python as rogue.interfaces.memory.Block()
+                *
+                * @param memBase Pointer to Slave memory device or Hub
+                * @param offset  Memory offset of the Block
+                * @param size    Memory size (footprint) of the Block
+                */
+               static std::shared_ptr<rogue::interfaces::memory::Block> create (uint64_t offset, uint32_t size);
+
+               // Setup class for use in python
+               static void setup_python();
+
+               // Create a Hub device with a given offset
+               Block (uint64_t offset, uint32_t size);
+
+               // Destroy the Hub
+               ~Block();
+
+               //! Return the path of the block
+               /** Return the path of the block in the device tree
+                *
+                * Exposed as path property to Python
+                * @return Full path of the block
+                */
+               std::string path();
+
+               //! Return the mode of the block
+               /** Return the mode of the block, the supported modes are
+                * "RW", "RO" and "WO"
+                *
+                * Exposed as mode property to Python
+                * @return Mode string
+                */
+               std::string mode();
+
+               //! Return bulk enable flag
+               /** Return the bulk enable flag, indicating if this block should be inclluded
+                * in bulk read and write operations.
+                *
+                * Exposed as bulkEn property to Python
+                * @return bulk enable flag
+                */
+               bool bulkEn();
+
+               //! Return overlap enable flag
+               /** Return the overlap enable flag
+                *
+                * Exposed as overlapEn property to Python
+                * @return overlap enable flag
+                */
+               bool overlapEn();
+
+               //! Return stale flag
+               /** Return the state sate flag.
+                *
+                * Exposed as stale property to Python
+                * @return stale state flag
+                */
+               bool stale();
+
+               //! Force the block to be stale
+               /** Force the internal stale state.
+                *
+                * Exposed as forceStale method to Python
+                */
+               void forceStale();
+
+               //! Clear the block stale state.
+               /** Clear the internal stale state.
+                *
+                * Exposed as clearStale method to Python
+                */
+               void clearStale();
+
+               //! Get offset of this Block
+               /** Return the offset of this Block
+                *
+                * Exposed as offset property to Python
+                * @return 64-bit address offset
+                */
+               uint64_t offset();
+
+               //! Get full address of this Block
+               /** Return the full address of this block, including the parent address plus
+                * the local offset.
+                *
+                * Exposed as address property to Python
+                * @return 64-bit address
+                */
+               uint64_t address();
+
+               //! Get size of this block in bytes.
+               /** Return the size of this block in bytes.
+                *
+                * Exposed as size property to Python
+                * @return 32-bit size
+                */
+               uint32_t size();
+
+               //! Get memory base id of this block
+               /** Return the ID of the memory slave this block is attached to.
+                *
+                * Exposed as memBaseId property to Python
+                * @return 32-bit memory base iD
+                */
+               uint32_t memBaseId();
+
+               //! Set value from RemoteVariable
+               /** Set the internal shadow memory with the requested variable value
+                *
+                * Exposed as set() method to Python
+                *
+                * @param var     Variable object this transaction is associated with
+                * @param value   Byte array containing variable value
+                */
+               void set(boost::python::object var, boost::python::object value);
+
+               //! Get value from RemoteVariable
+               /** Copy the shadow memory value into the passed byte array.
+                *
+                * Exposed as get() method to Python
+                *
+                * @param var     Variable object this transaction is associated with
+                * @param value   Byte array to copy data into
+                */
+               void get(boost::python::object var, boost::python::object value);
+
+               //! Start a transaction for this block
+               /** Start a transaction with the passed type and access range
+                *
+                * Exposed as startTransaction() method to Python
+                *
+                * @param type     Transaction type
+                * @param check    Flag to indicate if the transaction results should be immediately checked
+                * @param lowByte  Low byte of block to include in transaction.
+                * @param highByte High byte of block to include in transaction.
+                */
+               void startTransaction(uint32_t type, bool check, int32_t lowByte, int32_t highByte);
+
+               //! Check transaction result
+               /** Check transaction result, an exception is thrown if an error occured.
+                *
+                * Exposed as checkTransaction() method to Python
+                */
+               void checkTransaction();
+
+               //! Set default value
+               /** Set the default value and apply it to both shadow and staged memory
+                *
+                * Exposed as setDefault() method to Python
+                *
+                * @param var     Variable object to apply default to
+                * @param value   Byte array containing default value
+                */
+               void setDefault(boost::python::object var, boost::python::object value);
+
+
+               //! Call variable update for all variables
+               void varUpdate();
+
+               //! Add variables to block
+               /** Add the passed list of variables to this block
+                *
+                * Exposed as addVariables() method to Python
+                *
+                * @param variables Variable list
+                */
+               void addVariables(boost::python::object variables);
+         };
+
+         //! Alias for using shared pointer as BlockPtr
+         typedef std::shared_ptr<rogue::interfaces::memory::Block> BlockPtr;
+
+         class BlockVariable {
+            public:
+
+               boost::python::object var;
+
+               //std::string model;
+               //
+               std::string mode;
+
+               std::string name;
+
+               bool bulkEn;
+
+               bool overlapEn;
+
+               bool verify;
+
+               uint32_t varBytes;
+
+               //uint8_t * buffer;
+
+               uint32_t count;
+
+               uint32_t * bitOffset;
+
+               uint32_t * bitSize;
+         };
+
+         typedef std::shared_ptr<rogue::interfaces::memory::BlockVariable> BlockVariablePtr;
+      }
+   }
+}
+
+#endif
+#endif
+
---
+++ src/rogue/interfaces/memory/Block.cpp
@@ -0,0 +1,498 @@
+/**
+ *-----------------------------------------------------------------------------
+ * Title      : Memory Block
+ * ----------------------------------------------------------------------------
+ * File       : Block.cpp
+ * ----------------------------------------------------------------------------
+ * Description:
+ * Interface between RemoteVariables and lower level memory transactions.
+ * ----------------------------------------------------------------------------
+ * This file is part of the rogue software platform. It is subject to 
+ * the license terms in the LICENSE.txt file found in the top-level directory 
+ * of this distribution and at: 
+ *    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+ * No part of the rogue software platform, including this file, may be 
+ * copied, modified, propagated, or distributed except according to the terms 
+ * contained in the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+**/
+#include <rogue/interfaces/memory/Block.h>
+#include <rogue/interfaces/memory/Transaction.h>
+#include <rogue/interfaces/memory/Constants.h>
+#include <rogue/GilRelease.h>
+#include <rogue/ScopedGil.h>
+#include <rogue/GeneralError.h>
+#include <boost/python.hpp>
+#include <memory>
+#include <cmath>
+
+namespace rim = rogue::interfaces::memory;
+namespace bp  = boost::python;
+
+// Class factory which returns a pointer to a Block (BlockPtr)
+rim::BlockPtr rim::Block::create (uint64_t offset, uint32_t size) {
+   rim::BlockPtr b = std::make_shared<rim::Block>(offset,size);
+   return(b);
+}
+
+// Setup class for use in python
+void rim::Block::setup_python() {
+
+   bp::class_<rim::Block, rim::BlockPtr, bp::bases<rim::Master>, boost::noncopyable>("Block",bp::init<uint64_t,uint32_t>())
+       .add_property("path",      &rim::Block::path)
+       .add_property("mode",      &rim::Block::mode)
+       .add_property("bulkEn",    &rim::Block::bulkEn)
+       .add_property("overlapEn", &rim::Block::overlapEn)
+       .add_property("stale",     &rim::Block::stale)
+       .def("forceStale",         &rim::Block::forceStale)
+       .def("clearStale",         &rim::Block::clearStale)
+       .add_property("offset",    &rim::Block::offset)
+       .add_property("address",   &rim::Block::address)
+       .add_property("size",      &rim::Block::size)
+       .add_property("memBaseId", &rim::Block::memBaseId)
+       .def("set",                &rim::Block::set)
+       .def("get",                &rim::Block::get)
+       .def("startTransaction",   &rim::Block::startTransaction)
+       .def("checkTransaction",   &rim::Block::checkTransaction)
+       .def("setDefault",         &rim::Block::setDefault)
+       .def("addVariables",       &rim::Block::addVariables)
+   ;
+
+   bp::implicitly_convertible<rim::BlockPtr, rim::MasterPtr>();
+}
+
+// Create a Hub device with a given offset
+rim::Block::Block (uint64_t offset, uint32_t size) {
+   path_       = "Undefined";
+   mode_       = "RW";
+   bulkEn_     = false;
+   offset_     = offset;
+   size_       = size;
+   verifyEn_   = false;
+   verifyWr_   = false;
+   doUpdate_   = false;
+   blockTrans_ = false;
+
+   verifyBase_ = 0; // Verify Range
+   verifySize_ = 0; // Verify Range
+
+   stagedData_ = (uint8_t *)malloc(size_);
+   memset(stagedData_,0,size_);
+
+   stagedMask_ = (uint8_t *)malloc(size_);
+   memset(stagedMask_,0,size_);
+
+   blockData_ = (uint8_t *)malloc(size_);
+   memset(blockData_,0,size_);
+
+   verifyData_ = (uint8_t *)malloc(size_);
+   memset(verifyData_,0,size_);
+
+   verifyMask_ = (uint8_t *)malloc(size_);
+   memset(verifyMask_,0,size_);
+}
+
+// Destroy the Hub
+rim::Block::~Block() {
+   free(stagedData_);
+   free(stagedMask_);
+   free(blockData_);
+   free(verifyData_);
+   free(verifyMask_);
+}
+
+// Return the path of the block
+std::string rim::Block::path() {
+   return path_;
+}
+
+// Return the mode of the block
+std::string rim::Block::mode() {
+   return mode_;
+}
+
+// Return bulk enable flag
+bool rim::Block::bulkEn() {
+   return bulkEn_;
+}
+
+// Return overlap enable flag
+bool rim::Block::overlapEn() {
+   return overlapEn_;
+}
+
+// Return stale flag
+bool rim::Block::stale() {
+   uint32_t x;
+
+   rogue::GilRelease noGil;
+   std::lock_guard<std::mutex> lock(mtx_);
+
+   for (x=0; x < size_; x++) 
+      if ( stagedMask_[x] != 0 ) return true;
+
+   return false;
+}
+
+// Force the block to be stale
+void rim::Block::forceStale() {
+   uint32_t x;
+
+   if ( blockTrans_ ) return;
+
+   rogue::GilRelease noGil;
+   std::lock_guard<std::mutex> lock(mtx_);
+
+   for (x=0; x < size_; x++) {
+
+      // Copy only the unstale bits from the block
+      stagedData_[x] &= stagedMask_[x];
+      stagedData_[x] |= (blockData_[x] & (stagedMask_[x] ^ 0xFF));
+      
+      stagedMask_[x] = 0xFF;
+   }
+}
+
+// Clear the block stale state
+void rim::Block::clearStale() {
+   uint32_t x;
+
+   if ( blockTrans_ ) return;
+
+   rogue::GilRelease noGil;
+   std::lock_guard<std::mutex> lock(mtx_);
+
+   for (x=0; x < size_; x++) {
+      blockData_[x] &= (stagedMask_[x] ^ 0xFF);
+      blockData_[x] |= (stagedData_[x] & stagedMask_[x]);
+      stagedData_[x] = 0;
+      stagedMask_[x] = 0;
+   }
+}
+
+// Get offset of this Block
+uint64_t rim::Block::offset() {
+   return offset_;
+}
+
+// Get full address of this Block
+uint64_t rim::Block::address() {
+   return(reqAddress() | offset_);
+}
+
+// Get size of this block in bytes.
+uint32_t rim::Block::size() {
+   return size_;
+}
+
+// Get memory base id of this block
+uint32_t rim::Block::memBaseId() {
+   return(reqSlaveId());
+}
+
+// Set value from RemoteVariable
+void rim::Block::set(boost::python::object var, boost::python::object value) {
+   Py_buffer valueBuf;
+   uint8_t * data;
+   uint32_t  srcBit;
+   uint32_t  x;
+
+   if ( blockTrans_ ) return;
+
+   std::string name = bp::extract<std::string>(var.attr("name"));
+
+   rim::BlockVariablePtr bv = variables_[name];
+
+   if ( PyObject_GetBuffer(value.ptr(),&(valueBuf),PyBUF_SIMPLE) < 0 )
+      throw(rogue::GeneralError("Block::set","Python Buffer Error"));
+
+   data = (uint8_t *)valueBuf.buf;
+
+   rogue::GilRelease noGil;
+   std::lock_guard<std::mutex> lock(mtx_);
+
+   srcBit = 0;
+   for (x=0; x < bv->count; x++) {
+      copyBits(stagedData_, bv->bitOffset[x], data, srcBit, bv->bitSize[x]);
+      setBits(stagedMask_, bv->bitOffset[x], bv->bitSize[x]);
+      srcBit += bv->bitSize[x];
+   }
+}
+
+// Get value from RemoteVariable
+void rim::Block::get(boost::python::object var, boost::python::object value) {
+   Py_buffer valueBuf;
+   uint8_t * data;
+   uint32_t  dstBit;
+   uint32_t  x;
+
+   std::string name = bp::extract<std::string>(var.attr("name"));
+
+   rim::BlockVariablePtr bv = variables_[name];
+
+   if ( PyObject_GetBuffer(value.ptr(),&(valueBuf),PyBUF_SIMPLE) < 0 )
+      throw(rogue::GeneralError("Block::set","Python Buffer Error"));
+
+   data = (uint8_t *)valueBuf.buf;
+
+   rogue::GilRelease noGil;
+   std::lock_guard<std::mutex> lock(mtx_);
+
+   dstBit = 0;
+   for (x=0; x < bv->count; x++) {
+
+      if ( anyBits(stagedMask_, bv->bitOffset[x], bv->bitSize[x]) )
+         copyBits(data, dstBit, stagedData_, bv->bitOffset[x], bv->bitSize[x]);
+      else 
+         copyBits(data, dstBit, blockData_, bv->bitOffset[x], bv->bitSize[x]);
+
+      dstBit += bv->bitSize[x];
+   }
+}
+
+// Set default value
+void rim::Block::setDefault(boost::python::object var, boost::python::object value) {
+   Py_buffer valueBuf;
+   uint8_t * data;
+   uint32_t  srcBit;
+   uint32_t  x;
+
+   if ( blockTrans_ ) return;
+
+   std::string name = bp::extract<std::string>(var.attr("name"));
+
+   rim::BlockVariablePtr bv = variables_[name];
+
+   if ( PyObject_GetBuffer(value.ptr(),&(valueBuf),PyBUF_SIMPLE) < 0 )
+      throw(rogue::GeneralError("Block::set","Python Buffer Error"));
+
+   data = (uint8_t *)valueBuf.buf;
+
+   rogue::GilRelease noGil;
+   std::lock_guard<std::mutex> lock(mtx_);
+
+   srcBit = 0;
+   for (x=0; x < bv->count; x++) {
+      copyBits(stagedData_, bv->bitOffset[x], data, srcBit, bv->bitSize[x]);
+      setBits(stagedMask_,  bv->bitOffset[x], bv->bitSize[x]);
+      copyBits(blockData_,  bv->bitOffset[x], data, srcBit, bv->bitSize[x]);
+      srcBit += bv->bitSize[x];
+   }
+}
+
+// Start a transaction for this block
+void rim::Block::startTransaction(uint32_t type, bool check, int32_t lowByte, int32_t highByte) {
+   uint32_t  lowB;
+   uint32_t  highB;
+   uint32_t  x;
+   uint32_t  tOff;
+   uint32_t  tSize;
+   uint8_t * tData;
+
+   if ( blockTrans_ ) return;
+
+   // Check for valid combinations
+   if ( (type == rim::Write  and (mode_ == "RO")) ||
+        (type == rim::Post   and (mode_ == "RO")) ||
+        (type == rim::Read   and (mode_ == "WO")) ||
+        (type == rim::Verify and (mode_ == "WO" || mode_ == "RO" || !verifyWr_ ) ) ) return;
+
+   {
+      rogue::GilRelease noGil;
+      std::lock_guard<std::mutex> lock(mtx_);
+      waitTransaction(0);
+      clearError();
+
+      // Set default low and high bytes
+      lowB  = (lowByte  < 0)?0:lowByte;
+      highB = (highByte < 0)?size_-1:highByte;
+
+      // Move staged write data to block, clear stale
+      if ( type == rim::Write || type == rim::Post ) {
+         for (x=0; x < size_; x++) {
+            blockData_[x] &= (stagedMask_[x] ^ 0xFF);
+            blockData_[x] |= (stagedData_[x] & stagedMask_[x]);
+
+            // Adjust range to stale data
+            if ( stagedMask_[x] != 0 ) {
+               if ( x < lowB  ) lowB  = x;
+               if ( x > highB ) highB = x;
+            }
+
+            stagedData_[x] = 0;
+            stagedMask_[x] = 0;
+         }
+      }
+
+      bLog_->debug("Start transaction type = %i",type);
+
+      // Track verify after writes. 
+      // Only verify blocks that have been written since last verify
+      if ( type == rim::Write ) verifyWr_ = verifyEn_;
+
+      // Derive offset and size based upon min transaction size
+      tOff  = std::floor(lowB / reqMinAccess()) * reqMinAccess();
+      tSize = std::ceil((highB-lowB+1) / reqMinAccess()) * reqMinAccess();
+
+      // Setup transaction
+      if ( type == rim::Verify) {
+         verifyBase_ = tOff;
+         verifySize_ = tSize;
+         tData = verifyData_ + tOff;
+      } else {
+         verifyBase_ = 0;
+         verifySize_ = 0;
+         tData = blockData_ + tOff;
+      }
+      doUpdate_   = true;
+
+      // Start transaction, outside of lock
+      reqTransaction(offset_+tOff, tSize, tData, type);
+   }
+            
+   if ( check ) checkTransaction();
+}
+
+// Check transaction result
+void rim::Block::checkTransaction() {
+   std::string err;
+   bool locUpdate;
+   uint32_t x;
+
+   if ( blockTrans_ ) return;
+
+   {
+      rogue::GilRelease noGil;
+      std::lock_guard<std::mutex> lock(mtx_);
+      waitTransaction(0);
+
+      err = getError();
+      clearError();
+
+      if ( err != "" ) {
+         throw(rogue::GeneralError::create("Block::checkTransaction",
+            "Transaction error for block %s with address 0x%.8x. Error %s",
+            path_.c_str(), address(), err.c_str()));
+      }
+
+      if ( verifySize_ != 0 ) {
+         verifyWr_ = false;
+
+         for (x=verifyBase_; x < verifyBase_ + verifySize_; x++) {
+            if ((verifyData_[x] & verifyMask_[x]) != (blockData_[x] & verifyMask_[x])) {
+               throw(rogue::GeneralError::create("Block::checkTransaction",
+                  "Verify error for block %s with address 0x%.8x. Index: %i. Got: 0x%.2x, Exp: 0x%.2x, Mask: 0x%.2x",
+                  path_.c_str(), address(), x, verifyData_[x], blockData_[x], verifyMask_[x]));
+            }
+         }
+      }
+
+      locUpdate = doUpdate_;
+      doUpdate_ = false;
+   }
+
+   // Update variables outside of lock, GIL re-acquired
+   if ( locUpdate ) varUpdate();
+}
+
+// Call variable update for all variables
+void rim::Block::varUpdate() {
+   std::map<std::string, rim::BlockVariablePtr>::iterator vit;
+
+   rogue::ScopedGil gil;
+
+   for ( vit = variables_.begin(); vit != variables_.end(); ++vit ) 
+      vit->second->var.attr("_queueUpdate")();
+}
+
+// Add variables to block
+void rim::Block::addVariables(boost::python::object variables) {
+   char tmpBuff[100];
+
+   uint32_t x;
+   uint32_t y;
+
+   uint8_t excMask[size_];
+   uint8_t oleMask[size_];
+
+   memset(excMask,0,size_);
+   memset(oleMask,0,size_);
+
+   bp::list vl = (bp::list)variables;
+
+   for (x=0; x < bp::len(vl); x++) {
+
+      rim::BlockVariablePtr vb = std::make_shared<rim::BlockVariable>();
+
+      vb->var       = vl[x];
+      vb->varBytes  = bp::extract<uint32_t>(vb->var.attr("_bytes"));
+      vb->name      = std::string(bp::extract<char *>(vb->var.attr("_name")));
+      vb->mode      = std::string(bp::extract<char *>(vb->var.attr("_mode")));
+      vb->bulkEn    = bp::extract<bool>(vb->var.attr("_bulkEn"));
+      vb->overlapEn = bp::extract<bool>(vb->var.attr("_overlapEn"));
+      vb->verify    = bp::extract<bool>(vb->var.attr("_verify"));
+
+      vb->count     = len(vb->var.attr("_bitSize"));
+
+      vb->bitOffset = (uint32_t *)malloc(sizeof(uint32_t) * vb->count);
+      vb->bitSize   = (uint32_t *)malloc(sizeof(uint32_t) * vb->count);
+
+      variables_[vb->name] = vb;
+
+      if ( x == 0 ) {
+         path_ = std::string(bp::extract<char *>(vl[x].attr("_path")));
+         std::string logName = "memory.block." + path_;
+         bLog_ = rogue::Logging::create(logName.c_str());
+         mode_ = vb->mode;
+      }
+
+      if ( vb->bulkEn ) bulkEn_ = true;
+
+      bLog_->debug("Adding variable %s to block %s at offset 0x%.8x",vb->name.c_str(),path_.c_str(),offset_);
+
+      // If variable modes mismatch, set block to read/write
+      if ( mode_ != vb->mode ) mode_ = "RW";
+
+      // Update variable masks
+      for (y=0; y < vb->count; y++) {
+         vb->bitOffset[y] = bp::extract<uint32_t>(vb->var.attr("_bitOffset")[y]);
+         vb->bitSize[y]   = bp::extract<uint32_t>(vb->var.attr("_bitSize")[y]);
+
+         // Variable allows overlaps, add to overlap enable mask
+         if ( vb->overlapEn ) setBits(oleMask,vb->bitOffset[y],vb->bitSize[y]);
+
+         // Otherwise add to exclusive mask and check for existing mapping
+         else {
+            if (anyBits(excMask,vb->bitOffset[y],vb->bitSize[y])) 
+               throw(rogue::GeneralError::create("Block::addVariables",
+                     "Variable bit overlap detected for block %s with address 0x%.8x",
+                     path_.c_str(), address()));
+
+            setBits(excMask,vb->bitOffset[y],vb->bitSize[y]);
+         }
+
+         // update verify mask
+         if ( vb->mode == "RW" && vb->verify ) {
+            verifyEn_ = true;
+            setBits(verifyMask_,vb->bitOffset[y],vb->bitSize[y]);
+         }
+      } 
+   }
+
+   // Init overlap enable before check
+   overlapEn_ = true;
+
+   // Check for overlaps by anding exclusive and overlap bit vectors
+   for (x=0; x < size_; x++) {
+      if ( oleMask[x] & excMask[x] ) 
+         throw(rogue::GeneralError::create("Block::addVariables",
+               "Variable bit overlap detected for block %s with address 0x%.8x",
+               path_.c_str(), address()));
+
+      if ( excMask[x] != 0 ) overlapEn_ = false;
+   }
+
+   // Force block to be stale
+   forceStale();
+}
+
