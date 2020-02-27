82723d993d72cc1cc65275e3bb2c12ddf5be75d1
cryo <cryo@smurf-srv11>
2020-02-27 09:47:03 -0800

    Remove lock before calling epics update

--- src/rogue/protocols/epicsV3/Slave.cpp
+++ src/rogue/protocols/epicsV3/Slave.cpp
@@ -136,73 +136,75 @@ void rpe::Slave::acceptFrame ( ris::FramePtr frame ) {
    // Limit size
    if ( size_ > max_ ) size_ = max_;
 
-   boost::lock_guard<boost::mutex> lock(rpe::Value::mtx_);
+   {
+       boost::lock_guard<boost::mutex> lock(rpe::Value::mtx_);
 
-   // Release old data
-   pValue_->unreference();
-   pValue_ = new gddAtomic (gddAppType_value, epicsType_, 1u, size_);
+       // Release old data
+       pValue_->unreference();
+       pValue_ = new gddAtomic (gddAppType_value, epicsType_, 1u, size_);
 
-   // Set timestamp
+       // Set timestamp
 #ifdef __MACH__ // OSX does not have clock_gettime
-   clock_serv_t cclock;
-   mach_timespec_t mts;
-   host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-   clock_get_time(cclock, &mts);
-   mach_port_deallocate(mach_task_self(), cclock);
-   t.tv_sec = mts.tv_sec;
-   t.tv_nsec = mts.tv_nsec;
+       clock_serv_t cclock;
+       mach_timespec_t mts;
+       host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+       clock_get_time(cclock, &mts);
+       mach_port_deallocate(mach_task_self(), cclock);
+       t.tv_sec = mts.tv_sec;
+       t.tv_nsec = mts.tv_nsec;
 #else      
-   clock_gettime(CLOCK_REALTIME,&t);
+       clock_gettime(CLOCK_REALTIME,&t);
 #endif
-   pValue_->setTimeStamp(&t);
+       pValue_->setTimeStamp(&t);
 
-   // Create vector of appropriate type
-   if ( epicsType_ == aitEnumUint8 ) {
-      aitUint8 * pF = new aitUint8[size_];
-      for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
-      pValue_->putRef(pF, new rpe::Destructor<aitUint8 *>);
-   }
+       // Create vector of appropriate type
+       if ( epicsType_ == aitEnumUint8 ) {
+          aitUint8 * pF = new aitUint8[size_];
+          for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
+          pValue_->putRef(pF, new rpe::Destructor<aitUint8 *>);
+       }
 
-   else if ( epicsType_ == aitEnumUint16 ) {
-      aitUint16 * pF = new aitUint16[size_];
-      for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
-      pValue_->putRef(pF, new rpe::Destructor<aitUint16 *>);
-   }
+       else if ( epicsType_ == aitEnumUint16 ) {
+          aitUint16 * pF = new aitUint16[size_];
+          for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
+          pValue_->putRef(pF, new rpe::Destructor<aitUint16 *>);
+       }
 
-   else if ( epicsType_ == aitEnumUint32 ) {
-      aitUint32 * pF = new aitUint32[size_];
-      for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
-      pValue_->putRef(pF, new rpe::Destructor<aitUint32 *>);
-   }
+       else if ( epicsType_ == aitEnumUint32 ) {
+          aitUint32 * pF = new aitUint32[size_];
+          for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
+          pValue_->putRef(pF, new rpe::Destructor<aitUint32 *>);
+       }
 
-   else if ( epicsType_ == aitEnumInt8 ) {
-      aitInt8 * pF = new aitInt8[size_];
-      for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
-      pValue_->putRef(pF, new rpe::Destructor<aitInt8 *>);
-   }
+       else if ( epicsType_ == aitEnumInt8 ) {
+          aitInt8 * pF = new aitInt8[size_];
+          for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
+          pValue_->putRef(pF, new rpe::Destructor<aitInt8 *>);
+       }
 
-   else if ( epicsType_ == aitEnumInt16 ) {
-      aitInt16 * pF = new aitInt16[size_];
-      for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
-      pValue_->putRef(pF, new rpe::Destructor<aitInt16 *>);
-   }
+       else if ( epicsType_ == aitEnumInt16 ) {
+          aitInt16 * pF = new aitInt16[size_];
+          for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
+          pValue_->putRef(pF, new rpe::Destructor<aitInt16 *>);
+       }
 
-   else if ( epicsType_ == aitEnumInt32 ) {
-      aitInt32 * pF = new aitInt32[size_];
-      for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
-      pValue_->putRef(pF, new rpe::Destructor<aitInt32 *>);
-   }
+       else if ( epicsType_ == aitEnumInt32 ) {
+          aitInt32 * pF = new aitInt32[size_];
+          for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
+          pValue_->putRef(pF, new rpe::Destructor<aitInt32 *>);
+       }
 
-   else if ( epicsType_ == aitEnumFloat32 ) {
-      aitFloat32 * pF = new aitFloat32[size_];
-      for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
-      pValue_->putRef(pF, new rpe::Destructor<aitFloat32 *>);
-   }
+       else if ( epicsType_ == aitEnumFloat32 ) {
+          aitFloat32 * pF = new aitFloat32[size_];
+          for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
+          pValue_->putRef(pF, new rpe::Destructor<aitFloat32 *>);
+       }
 
-   else if ( epicsType_ == aitEnumFloat64 ) {
-      aitFloat64 * pF = new aitFloat64[size_];
-      for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
-      pValue_->putRef(pF, new rpe::Destructor<aitFloat64 *>);
+       else if ( epicsType_ == aitEnumFloat64 ) {
+          aitFloat64 * pF = new aitFloat64[size_];
+          for (i=0; i < size_; ++i) fromFrame(iter,fSize_,&(pF[i]));
+          pValue_->putRef(pF, new rpe::Destructor<aitFloat64 *>);
+       }
    }
 
    updated();
