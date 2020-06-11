feef5de292220e29173d173f5ea65c7d2343f1b2
Ryan Herbst <rherbst@slac.stanford.edu>
2020-06-11 01:04:25 -0700

    Fix library paths

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -7,9 +7,9 @@ make -j ${CPU_COUNT} install
 
 mkdir -p $PREFIX/etc/conda/activate.d
 cat <<EOF > $PREFIX/etc/conda/activate.d/rogue_activate.sh
-export EPICS_BASE_LIB="${EPICS_BASE}/lib/${EPICS_HOST_ARCH}"
-export EPICS_PCAS_LIB="${EPICS_PCAS_ROOT}/lib/${EPICS_HOST_ARCH}"
-export LD_LIBRARY_PATH=${EPICS_BASE_LIB}:${EPICS_PCAS_LIB}${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+export EPICS_BASE_LIB="\${CONDA_PREFIX}/epics/lib/${EPICS_HOST_ARCH}"
+export EPICS_PCAS_LIB="\${CONDA_PREFIX}/pcas/lib/${EPICS_HOST_ARCH}"
+export LD_LIBRARY_PATH=\${EPICS_BASE_LIB}:\${EPICS_PCAS_LIB}\${LD_LIBRARY_PATH:+:\$LD_LIBRARY_PATH}
 EOF
 
 mkdir -p $PREFIX/etc/conda/deactivate.d
