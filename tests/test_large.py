ba0b3be5b5cdb85fcd9e10721d0e4f500825bcb1
Ryan Herbst <rherbst@slac.stanford.edu>
2020-09-15 00:00:19 -0700

    Add large device

---
+++ tests/test_large.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+# Title      : Server only test script
+#-----------------------------------------------------------------------------
+# This file is part of the rogue_example software. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the rogue_example software, including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+import pyrogue
+
+class TestLarge(pyrogue.Device):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+        for i in range(4096):
+            self.add(pyrogue.LocalVariable(
+                name = f'TestValue[{i}]',
+                mode = 'RW',
+                value = 0))
