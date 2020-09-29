3c20badc08aabdb76e078023f7322a01658277cf
Ryan Herbst <rherbst@slac.stanford.edu>
2020-09-29 09:36:40 -0700

    Add slac header

--- tests/epics_client_testing.py
+++ tests/epics_client_testing.py
@@ -1,4 +1,15 @@
-
+#!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+# Title      : EPICS Testing Script
+#-----------------------------------------------------------------------------
+# This file is part of the rogue_example software. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the rogue_example software, including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
 import epics
 import time
 import pyrogue.interfaces
