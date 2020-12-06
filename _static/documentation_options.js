2ba2ca5c8403eeb8bffca464de2db159ee0f56a6
slacrherbst <slacrherbst@users.noreply.github.com>
2020-12-06 05:00:19 +0000

    deploy: 841a7feb9d0c0708ca8d959137c5271e4308bbcd

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v5.6.1-101-ge6695605',
+    VERSION: 'v5.6.1-102-g841a7feb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
