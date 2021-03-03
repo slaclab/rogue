90d8a6b6b25b24c9888723a884346fd9ae982b08
slacrherbst <slacrherbst@users.noreply.github.com>
2021-03-03 22:37:38 +0000

    deploy: 71d0c94306d0abbfa288680f95dd3a6278470953

--- _sources/utilities/fileio/index.rst.txt
+++ _sources/utilities/fileio/index.rst.txt
@@ -4,7 +4,12 @@
 File I/O Utilities
 ==================
 
+The following are examples of using the fileio utilities in Rogue to write and read data files.
 
 .. toctree::
    :maxdepth: 1
    :caption: Using The File I/O:
+
+   writing
+   reading
+   custom
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v5.6.5-17-ge74a8f2b',
+    VERSION: 'v5.6.5-18-g71d0c943',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
