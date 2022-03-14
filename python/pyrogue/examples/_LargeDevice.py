a4f88ddfe894718e24df75525dee445f72904133
Ryan Herbst <rherbst@slac.stanford.edu>
2022-03-14 00:19:10 -0700

    Update

---
+++ python/pyrogue/examples/_LargeDevice.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+# Title      : Large Device Test Group
+#-----------------------------------------------------------------------------
+# This file is part of the rogue_example software. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the rogue_example software, including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+import pyrogue
+
+class LargeDevice(pyrogue.Device):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+        for i in range(4096):
+            b = (i // 100) * 100
+            t = (i // 100) * 100 + 99
+            self.add(pyrogue.LocalVariable(
+                name = f'TestValue[{i}]',
+                mode = 'RW',
+                guiGroup=f'TestLargeGroup[{b}-{t}]',
+                value = 0))
