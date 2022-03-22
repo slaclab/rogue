2366eb651885c032747a6bf0b0fb6b63bb5807cf
Michael Skoufis <skoufis@pc94331.slac.stanford.edu>
2022-03-22 09:11:02 -0700

    Preliminary integration of xvc server

---
+++ include/rogue/protocols/xilinx/xvc/XvcConn.h
@@ -0,0 +1,77 @@
+//-----------------------------------------------------------------------------
+// Title      : JTAG Support
+//-----------------------------------------------------------------------------
+// Company    : SLAC National Accelerator Laboratory
+//-----------------------------------------------------------------------------
+// Description: 
+//-----------------------------------------------------------------------------
+// This file is part of 'SLAC Firmware Standard Library'.
+// It is subject to the license terms in the LICENSE.txt file found in the 
+// top-level directory of this distribution and at: 
+//    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+// No part of 'SLAC Firmware Standard Library', including this file, 
+// may be copied, modified, propagated, or distributed except according to 
+// the terms contained in the LICENSE.txt file.
+//-----------------------------------------------------------------------------
+
+#ifndef XVC_CONNECTION_H
+#define XVC_CONNECTION_H
+
+#include <rogue/protocols/xilinx/xvc/XvcSrv.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+namespace rogue {
+   namespace protocols {
+      namespace xilinx {
+         namespace xvc {
+
+// Class managing a XVC tcp connection
+
+class XvcConn {
+	JtagDriver        *drv_;
+	int                sd_;
+	struct sockaddr_in peer_;
+	// just use vectors to back raw memory; DONT use 'size/resize'
+	// (unfortunately 'resize' fills elements beyond the current 'size'
+	// with zeroes)
+	vector<uint8_t>    rxb_;
+	uint8_t           *rp_;
+    unsigned long      rl_;
+	unsigned long      tl_;
+
+	vector<uint8_t>    txb_;
+	unsigned long      maxVecLen_;
+	unsigned long      supVecLen_;
+	unsigned long      chunk_;
+
+public:
+XvcConn( int sd, JtagDriver *drv, unsigned long maxVecLen_ = 32768 );
+
+        //! Setup class in python
+        static void setup_python();
+        
+	// fill rx buffer to 'n' octets (from TCP connection)
+	virtual void fill(unsigned long n);
+
+	// send tx buffer to TCP connection
+	virtual void flush();
+
+	// discard 'n' octets from rx buffer (mark as consumed)
+	virtual void bump(unsigned long n);
+
+	// (re)allocated buffers
+	virtual void allocBufs();
+
+	virtual void run();
+
+	virtual ~XvcConn();
+};
+
+         }
+      }
+   }
+}
+
+#endif
---
+++ src/rogue/protocols/xilinx/xvc/module.cpp
@@ -0,0 +1,49 @@
+/**
+ *-----------------------------------------------------------------------------
+ * Title      : Python Module
+ * ----------------------------------------------------------------------------
+ * File       : module.cpp
+ * Author     : Ryan Herbst, rherbst@slac.stanford.edu
+ * Created    : 2016-08-08
+ * Last update: 2016-08-08
+ * ----------------------------------------------------------------------------
+ * Description:
+ * Python module setup
+ * ----------------------------------------------------------------------------
+ * This file is part of the rogue software platform. It is subject to
+ * the license terms in the LICENSE.txt file found in the top-level directory
+ * of this distribution and at:
+ *    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ * No part of the rogue software platform, including this file, may be
+ * copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+**/
+
+#define BOOST_BIND_GLOBAL_PLACEHOLDERS
+#include <boost/python.hpp>
+#include <rogue/protocols/xilinx/xvc/module.h>
+#include <rogue/protocols/xilinx/xvc/XvcConn.h>
+#include <rogue/protocols/xilinx/xvc/XvcDrvAxisFifo.h>
+#include <rogue/protocols/xilinx/xvc/XvcDrvLoopBack.h>
+#include <rogue/protocols/xilinx/xvc/XvcDrvUdp.h>
+#include <rogue/protocols/xilinx/xvc/XvcSrv.h>
+
+namespace bp  = boost::python;
+namespace rpxx = rogue::protocols::xilinx::xvc;
+
+void rpxx::setup_module() {
+
+   // map the IO namespace to a sub-module
+   bp::object module(bp::handle<>(bp::borrowed(PyImport_AddModule("rogue.protocols.xilinx.xvc"))));
+
+   // make "from mypackage import class1" work
+   bp::scope().attr("xilinx.xvc") = module;
+
+   // set the current scope to the new sub-module
+   bp::scope io_scope = module;
+
+   rpxx::XvcConn::setup_python();
+   rpxx::XvcServer::setup_python();
+}
+
