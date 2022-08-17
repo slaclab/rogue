8c5576977230a0b62ff27531f4697f172bf342c3
Harrison Nicholls <nichollsharrisonj@gmail.com>
2022-08-17 15:32:55 -0700

    typo fix

--- python/pyrogue/pydm/widgets/time_plotter.py
+++ python/pyrogue/pydm/widgets/time_plotter.py
@@ -469,7 +469,7 @@ class TimePlotter(PyDMFrame):
         selection_layout = QVBoxLayout()
         # selection_layout.resize(QtCore.QSize(1500, 1000))
         self.selection_tree = SelectionTree(main=self,parent=None,init_channel=self.channel)
-        self.selection_tree.setWidth(650)
+        self.selection_tree.setMinimumWidth(680)
         selection_layout.addWidget(self.selection_tree)
         selection_layout.addWidget(self.scroll_area)
         selection_box = QGroupBox()
