24978e86f9b8c4c4c72707e6dd727ed20b0adf97
Benjamin Reese <bareese@slac.stanford.edu>
2022-09-01 10:40:41 -0700

    Fix bug in start and stop recursion

--- python/pyrogue/_Device.py
+++ python/pyrogue/_Device.py
@@ -238,7 +238,7 @@ class Device(pr.Node,rim.Hub):
         for intf in self._ifAndProto:
             if hasattr(intf,"_start"):
                 intf._start()
-        for d in self.deviceList:
+        for d in self.devices.values():
             d._start()
 
     def _stop(self):
@@ -246,7 +246,7 @@ class Device(pr.Node,rim.Hub):
         for intf in self._ifAndProto:
             if hasattr(intf,"_stop"):
                 intf._stop()
-        for d in self.deviceList:
+        for d in self.devices.values():
             d._stop()
 
     @property
