d72262c57ce9ee9da87dd113c207af35c42fd412
Ryan Herbst <rherbst@slac.stanford.edu>
2023-04-18 10:58:58 -0700

    Update example calls

--- tests/api_test/src/api_test.cpp
+++ tests/api_test/src/api_test.cpp
@@ -8,16 +8,24 @@ int main (int argc, char **argv) {
    printf("Root started\n");
 
    // Using non pointer references
+
+   // Get running uptime clock
    printf("LocalTime = %s\n",root["LocalTime"].getDisp().c_str());
+
+   // Set and get scratchpad
    root["AxiVersion"]["ScratchPad"].setDisp("0x1111");
    printf("ScratchPad = %s\n",root["AxiVersion"]["ScratchPad"].getDisp().c_str());
 
-   // Get object as a pointer
+   // Get object as a pointer using full path, and get scratchpad
    printf("ScratchPad = %s\n",root.getNode("ExampleRoot.AxiVersion.ScratchPad")->getDisp().c_str());
 
    // Get yaml config
    std::string cfg = root["GetYamlConfig"].call("True");
    printf("Config = %s\n", cfg.c_str());
+
+   // Set yaml config, example
+   //root["SetYamlConfig"].call("Some Yaml String");
+
    sleep(3);
 
    root.stop();
