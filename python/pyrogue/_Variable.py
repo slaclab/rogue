1c66138ff0b738848e5498b5d09fdcd7222219e0
Ryan Herbst <rherbst@slac.stanford.edu>
2023-04-21 16:08:44 -0700

    Disable disp generation for some variables

--- python/pyrogue/_Variable.py
+++ python/pyrogue/_Variable.py
@@ -602,6 +602,9 @@ class BaseVariable(pr.Node):
         """
         try:
 
+            if useDisp == '':
+                return ''
+
             if isinstance(value,np.ndarray):
                 return np.array2string(value,
                                        separator=', ',
