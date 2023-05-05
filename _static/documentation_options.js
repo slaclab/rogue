6ca2d8d71d5a9c4a186f9d66b6c387b8f0195b08
slacrherbst <slacrherbst@users.noreply.github.com>
2023-05-05 05:02:56 +0000

    deploy: 6973d07cb43c1675de52848f5e3c74d8297faa88

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v5.18.3',
+    VERSION: 'v5.18.4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
