d8948a39a3dcd940b957f729ea825949c7b7bc42
Benjamin Reese <bareese@slac.stanford.edu>
2023-05-19 11:22:19 -0700

    Add license header

--- tests/api_test/src/api_test.cpp
+++ tests/api_test/src/api_test.cpp
@@ -1,3 +1,14 @@
+/* ----------------------------------------------------------------------------
+ * This file is part of the rogue software platform. It is subject to
+ * the license terms in the LICENSE.txt file found in the top-level directory
+ * of this distribution and at:
+ *    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ * No part of the rogue software platform, including this file, may be
+ * copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+**/
+
 #include <rogue/interfaces/api/Bsp.h>
 
 void varListener(std::string path, std::string value) {
