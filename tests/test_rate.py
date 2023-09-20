57eeb339e289972390b5b39e0db25f5ed2506ccc
Ryan Herbst <rherbst@slac.stanford.edu>
2023-09-19 21:05:53 -0700

    Show rate as float

--- tests/test_rate.py
+++ tests/test_rate.py
@@ -21,13 +21,13 @@ import hwcounter
 #logger = logging.getLogger('pyrogue')
 #logger.setLevel(logging.DEBUG)
 
-MaxCycles = { 'remoteSetRate'   : 42000,
-              'remoteSetNvRate' : 46000,
-              'remoteGetRate'   : 54000,
-              'localSetRate'    : 36000,
-              'localGetRate'    : 55000,
-              'linkedSetRate'   : 36000,
-              'linkedGetRate'   : 45000 }
+MaxCycles = { 'remoteSetRate'   : 4.2e4,
+              'remoteSetNvRate' : 4.6e4,
+              'remoteGetRate'   : 5.4e4,
+              'localSetRate'    : 3.6e4,
+              'localGetRate'    : 5.5e4,
+              'linkedSetRate'   : 3.6e4,
+              'linkedGetRate'   : 4.5e4 }
 
 class TestDev(pr.Device):
 
@@ -115,7 +115,7 @@ def test_rate():
         with root.updateGroup():
             for i in range(count):
                 root.TestDev.TestRemote.set(i)
-        resultCycles['remoteSetRate'] = hwcounter.count_end() - scount
+        resultCycles['remoteSetRate'] = float(hwcounter.count_end() - scount)
         resultRate['remoteSetRate'] = int(1/((time.time()-stime) / count))
 
         stime = time.time()
@@ -123,7 +123,7 @@ def test_rate():
         with root.updateGroup():
             for i in range(count):
                 root.TestDev.TestRemoteNoVerify.set(i)
-        resultCycles['remoteSetNvRate'] = hwcounter.count_end() - scount
+        resultCycles['remoteSetNvRate'] = float(hwcounter.count_end() - scount)
         resultRate['remoteSetNvRate'] = int(1/((time.time()-stime) / count))
 
         stime = time.time()
@@ -131,7 +131,7 @@ def test_rate():
         with root.updateGroup():
             for i in range(count):
                 root.TestDev.TestRemote.get()
-        resultCycles['remoteGetRate'] = hwcounter.count_end() - scount
+        resultCycles['remoteGetRate'] = float(hwcounter.count_end() - scount)
         resultRate['remoteGetRate'] = int(1/((time.time()-stime) / count))
 
         stime = time.time()
@@ -139,7 +139,7 @@ def test_rate():
         with root.updateGroup():
             for i in range(count):
                 root.TestDev.TestLocal.set(i)
-        resultCycles['localSetRate'] = hwcounter.count_end() - scount
+        resultCycles['localSetRate'] = float(hwcounter.count_end() - scount)
         resultRate['localSetRate'] = int(1/((time.time()-stime) / count))
 
         stime = time.time()
@@ -147,7 +147,7 @@ def test_rate():
         with root.updateGroup():
             for i in range(count):
                 root.TestDev.TestLocal.get()
-        resultCycles['localGetRate'] = hwcounter.count_end() - scount
+        resultCycles['localGetRate'] = float(hwcounter.count_end() - scount)
         resultRate['localGetRate'] = int(1/((time.time()-stime) / count))
 
         stime = time.time()
@@ -155,7 +155,7 @@ def test_rate():
         with root.updateGroup():
             for i in range(count):
                 root.TestDev.TestLink.set(i)
-        resultCycles['linkedSetRate'] = hwcounter.count_end() - scount
+        resultCycles['linkedSetRate'] = float(hwcounter.count_end() - scount)
         resultRate['linkedSetRate'] = int(1/((time.time()-stime) / count))
 
         stime = time.time()
@@ -163,14 +163,14 @@ def test_rate():
         with root.updateGroup():
             for i in range(count):
                 root.TestDev.TestLink.get(i)
-        resultCycles['linkedGetRate'] = hwcounter.count_end() - scount
+        resultCycles['linkedGetRate'] = float(hwcounter.count_end() - scount)
         resultRate['linkedGetRate'] = int(1/((time.time()-stime) / count))
 
         passed = True
 
         for k,v in MaxCycles.items():
 
-            print(f"{k} cyles {resultCycles[k]}, maximum {v}, rate {resultRate[k]}")
+            print(f"{k} cyles {resultCycles[k]:.2e}, maximum {v:.2e}, rate {resultRate[k]}")
 
             if resultCycles[k] > v:
                 passed = False
