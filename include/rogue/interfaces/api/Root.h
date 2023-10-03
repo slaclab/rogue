713948dee8655eb61048d9829c84404c83191e84
Omar Moreno <email@omarmoreno.net>
2023-10-02 23:56:36 -0700

    Add list of commands, wrap start and stop functions.

--- include/rogue/interfaces/api/Root.h
+++ include/rogue/interfaces/api/Root.h
@@ -1,21 +1,43 @@
-
 #pragma once
 
 #include <boost/python.hpp>
+
 #include "rogue/interfaces/api/Node.h" 
+#include "rogue/interfaces/api/Command.h" 
 
 namespace rogue::interfaces::api { 
-
 class Root : Node {
 
  public: 
-  //
-  Root(boost::python::object &obj);
-  ~Root() = default;  
+  //! Constructor which takes a boost::python::object representing the root
+  //! of a tree of nodes.
+  //! 
+  //! @param[in] obj Boost python object representing the root of a tree of
+  //!   nodes.
+  Root(const boost::python::object &obj);
+
+  //! Default constuctor
+  ~Root() = default; 
+
+   //! Setup the tree and start the polling thread.
+   void start();
+
+   //! Stop the polling thread. This function must be called for a clean exit.
+   void stop();   
+
+   //! Add a variable updated listener function.
+   void addVarListener(void (*func)(std::string, std::string), void (*done)());
+
+   //! Load a YAML configuration from a file.
+   bool loadYaml(std::string &name); 
 
   private:
 
-  std::vector<Node> _nodes;
+   //! List of nodes in the tree
+   std::vector<Node> _nodes;
+
+   //! List of commands in the tree
+   std::vector<Command> _commands; 
 
 };
 
