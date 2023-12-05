b36cd5597c5088abb98fe4f623f201db3b39a4aa
Omar Moreno <email@omarmoreno.net>
2023-12-05 00:16:17 -0800

    Class representing a node in the device tree. This wraps pyrogue.Node.

---
+++ include/rogue/interfaces/api/Node.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <boost/python.hpp>
+
+#include <map>
+#include <string>
+
+namespace rogue::interfaces::api { 
+class Node { 
+ public: 
+    /**
+     * Constructor
+     * @param[in] obj Boost wrapped python object representing a pyrogue.Node 
+     *  object.
+     */
+    Node(const boost::python::object &obj);
+
+    //! Destructor
+    ~Node() = default;
+
+ protected: 
+    //! Boost python object representing this node
+    boost::python::object _obj;
+
+ private: 
+    //! Map of child nodes
+    std::map<std::string, Node> _nodes;
+
+    //! Global name of the object
+    std::string _name{""};
+    
+    //! Description of a variable
+    std::string _description{""};
+
+    //! Full path to the node (e.g. node1.node2.node3)
+    std::string _path{""}; 
+
+    //! String representation of this class
+    friend auto operator<<(std::ostream os, Node const& node) -> std::ostream&; 
+      	
+};
+}
---
+++ src/rogue/interfaces/api/Node.cpp
@@ -0,0 +1,24 @@
+#include "rogue/interfaces/api/Node.h" 
+
+namespace rogue::interfaces::api { 
+
+Node::Node(const boost::python::object &obj) { 
+    _obj = obj;
+
+    _name = boost::python::extract<std::string>(obj.attr("_name"));
+    _description = boost::python::extract<std::string>(obj.attr("_description"));
+    _path = boost::python::extract<std::string>(obj.attr("_path"));
+
+}
+
+
+auto operator<<(std::ostream os, Node const& node) -> std::ostream& { 
+
+    os << "{ Node name: " << node._name << "\n"; 
+    os << "\tDescription: " << node._description << "\n"; 
+    os << "\tPath: " << node._path << "\n"; 
+    os << "}"; 
+    return os;
+}
+
+}
