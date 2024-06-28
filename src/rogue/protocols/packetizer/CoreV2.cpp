9e385b8bb85743842002e9b825a506499be549de
Larry Ruckman <ruckman@slac.stanford.edu>
2024-06-28 13:10:46 -0700

    resolving c++ linter
    Line contains only semicolon. If this should be an empty statement, use {} instead.  [whitespace/semicolon]

--- src/rogue/protocols/packetizer/CoreV2.cpp
+++ src/rogue/protocols/packetizer/CoreV2.cpp
@@ -49,9 +49,7 @@ void rpp::CoreV2::setup_python() {
     bp::class_<rpp::CoreV2, rpp::CoreV2Ptr, boost::noncopyable>("CoreV2", bp::init<bool, bool, bool>())
         .def("transport", &rpp::CoreV2::transport)
         .def("application", &rpp::CoreV2::application)
-        .def("getDropCount", &rpp::CoreV2::getDropCount)
-
-        ;
+        .def("getDropCount", &rpp::CoreV2::getDropCount);
 #endif
 }
 
