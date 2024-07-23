f5287b810ec6f032e8f1dd57122b02f42393e526
ruck314 <ruck314@users.noreply.github.com>
2024-07-23 04:30:37 +0000

    deploy: 50f80b4113bfc92d136db3397f63040c23ec4078

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v6.3.0',
+    VERSION: 'v6.4.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
