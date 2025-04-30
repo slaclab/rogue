381ac44cdc9fb6cbe576c7ee154935eb38cc8a11
Shawn W. Henderson <20823858+swh76@users.noreply.github.com>
2025-04-30 11:13:38 -0700

    collections.Iterable alias removed in python 3.10

--- python/pyrogue/_Variable.py
+++ python/pyrogue/_Variable.py
@@ -20,7 +20,7 @@ import re
 import time
 import numpy
 from collections import OrderedDict as odict
-from collections import Iterable
+from collections.abc import Iterable
 
 class VariableError(Exception):
     """ Exception for variable access errors."""
