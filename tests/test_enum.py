154b0ee1098b8dbb1624f791709b2a81ccdd2202
Larry Ruckman <ruckman@slac.stanford.edu>
2025-09-15 13:24:46 -0700

    updates so CI pytest can run in paralle

--- tests/test_enum.py
+++ tests/test_enum.py
@@ -69,14 +69,14 @@ class DummyTree(pr.Root):
         self.addInterface(sim)
 
         # Create a memory gateway
-        ms = rogue.interfaces.memory.TcpServer("127.0.0.1",9080)
+        ms = rogue.interfaces.memory.TcpServer("127.0.0.1",9010)
         self.addInterface(ms)
 
         # Connect the memory gateways together
         sim << ms
 
         # Create a memory gateway
-        mc = rogue.interfaces.memory.TcpClient("127.0.0.1",9080)
+        mc = rogue.interfaces.memory.TcpClient("127.0.0.1",9010)
         self.addInterface(mc)
 
         # Add Device
