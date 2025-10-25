90e3864d9857e02b56135e5df364b77174ba7007
Ryan Herbst <rherbst@slac.stanford.edu>
2025-10-24 17:25:49 -0700

    New test

--- src/rogue/interfaces/memory/Block.cpp
+++ src/rogue/interfaces/memory/Block.cpp
@@ -60,7 +60,7 @@ rim::BlockPtr rim::Block::create(uint64_t offset, uint32_t size) {
 #ifndef NO_PYTHON
 // Setup class for use in python
 void rim::Block::setup_python() {
-   _import_array();
+    _import_array();
 
     bp::class_<rim::Block, rim::BlockPtr, bp::bases<rim::Master>, boost::noncopyable>("Block",
                                                                                       bp::init<uint64_t, uint32_t>())
