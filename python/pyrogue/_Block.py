343e983803bd0b730589dc407426e9b7bf63672c
Ryan Herbst <rherbst@slac.stanford.edu>
2025-11-03 21:54:34 -0800

    Fix indent"

--- python/pyrogue/_Block.py
+++ python/pyrogue/_Block.py
@@ -253,7 +253,7 @@ class LocalBlock(object):
             else:
                 if not isinstance(value, var._nativeType):
                     self._log.warning( f'{var.path}: Expecting {var._nativeType}: Currently a warning but in the future this will be an error' )
-                     #raise TypeError(f"Error - {var.path}: Expecting {var._nativeType} but got {type(value)}")
+                    #raise TypeError(f"Error - {var.path}: Expecting {var._nativeType} but got {type(value)}")
 
                 self._value = value
 
