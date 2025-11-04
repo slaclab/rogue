360892f9603718ad69ff48558439a261579b2678
Larry Ruckman <ruckman@slac.stanford.edu>
2025-11-04 08:12:51 -0800

    adding code comment

--- scripts/run_linters.sh
+++ scripts/run_linters.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+# Note: Must run as `bash scripts/run_linters.sh` or  `./scripts/run_linters.sh` when using set -e
 set -e
 
 SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
@@ -10,4 +12,3 @@ cd ${SCRIPT_DIR}/..; python -m compileall -f ./tests
 cd ${SCRIPT_DIR}/..; flake8 --count ./tests/
 
 cd ${SCRIPT_DIR}/..; find . -name '*.h' -o -name '*.cpp' | grep -v build | xargs cpplint
-
